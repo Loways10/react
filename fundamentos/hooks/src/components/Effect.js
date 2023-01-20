5d6832f65f4acd7098592424b0a1d3abea4232fc
hattori_akio@hotmail.com <hattori_akio@hotmail.com>
2023-01-19 21:31:48 -0300

    Subindo aula 03 de React, hook e imagens

---
+++ fundamentos/hooks/src/components/Effect.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+
+const Effect = () => {
+    const [nome, setNome] = useState()
+    const [salario, setSalario] = useState(1000)
+    const [vendas, setVendas] = useState(0)
+
+    useEffect(() => {
+        console.log('Utilizando useEffect')
+    }, [])
+
+    useEffect(() => {
+       document.title = nome
+    }, [nome])
+
+    useEffect(() => {
+        setSalario( s => s + (vendas * 1.89))
+    }, [vendas])
+
+    return(
+        <div>
+            <h2>useEffect</h2>
+            <div>
+                <p>{nome}</p>
+                <div>
+                    <input type="text" onChange={(e) => setNome(e.target.value)} />
+                </div>
+            </div>
+
+            <div>
+                <p>R$ {salario.toFixed(2)}</p>
+                <div>
+                    <button onClick={() => setVendas(vendas + 1)}>Vendas: qnt {vendas}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Effect
---
+++ fundamentos/image/src/App.js
@@ -0,0 +1,21 @@
+// import Logo from './assets/logo512.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      { /* Pasta Public */}
+      <img src="/favicon.ico" alt="" />
+
+      { /* Pasta src */}
+      {/* <img src={Logo} alt="" /> */}
+
+      { /* LINK */ }
+      <img src="" alt="" />
+
+    </div>
+  );
+}
+
+export default App;
